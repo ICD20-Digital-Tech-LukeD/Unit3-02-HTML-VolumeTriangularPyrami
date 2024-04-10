ea14bb80411aad1f2b3ee19341a2446055d177d5
LukeDiBert <159164688+LukeDiBert@users.noreply.github.com>
2024-04-10 00:35:54 +0000

    ESFEesf

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+
+function volumeCalculator(){
+  // Get the values of the input fields
+  let sideA = document.getElementById("side-a").value;
+  let sideB = document.getElementById("side-b").value;
+  let sideC = document.getElementById("side-c").value;
+
+  // Calculate the volume
+  let volume = 1/6 * sideA * sideB * sideC
+
+  // Display the result
+  document.getElementById("result").innerHTML = "The volume of the triangular pyramid is " + volume.toFixed(2) + " cm<sup>3</sup>.";
+}
